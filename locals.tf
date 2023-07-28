6d3201abcdeba31f30b54291ef5876d412b6eb3a
lindsaygelle <lindsay.gelle@gmail.com>
2023-07-28 15:41:32 +1000

    Dev

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  environment = terraform.workspace == "default" ? "production" : terraform.workspace
+}
+locals {
+  # tags_all are tags to be assigned to all resources.
+  tags_all = {
+    application = var.application
+    environment = local.environment
+    region = var.region
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "account" {
+  type = string
+}
+
+variable "application" {
+  default = "FunkyMode"
+  type = string
+}
+
+variable "region" {
+  default = "us-east-1"
+  type    = string
+}
