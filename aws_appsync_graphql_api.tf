25fc6c60288f9976072c7013a3ab6231dea4bc92
lindsaygelle <lindsay.gelle@gmail.com>
2023-08-01 23:01:51 +1000

    Dev

--- aws_appsync_graphql_api.tf
+++ aws_appsync_graphql_api.tf
@@ -1,7 +1,12 @@
 resource "aws_appsync_graphql_api" "main" {
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
-  name                = var.application
-  tags_all            = local.tags_all
+  log_config {
+    cloudwatch_logs_role_arn = aws_iam_role.app_sync.arn
+    exclude_verbose_content  = false
+    field_log_level          = "ALL"
+  }
+  name     = var.application
+  tags_all = local.tags_all
   user_pool_config {
     aws_region     = var.region
     default_action = "DENY"
