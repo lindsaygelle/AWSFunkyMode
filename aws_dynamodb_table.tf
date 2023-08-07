09b82022f9acba08edf0780af6357a1e7903fadb
Linneeeeeeee <lindsay.gelle@gmail.com>
2023-08-07 16:15:35 +1000

    Dev (#9)

--- aws_dynamodb_table.tf
+++ aws_dynamodb_table.tf
@@ -1,14 +1,19 @@
 resource "aws_dynamodb_table" "main" {
   attribute {
-    name = "Id"
+    name = "id"
+    type = "S"
+  }
+  attribute {
+    name = "sk"
     type = "S"
   }
   billing_mode = "PROVISIONED"
-  hash_key     = "Id"
+  hash_key     = "id"
   name         = var.application
   point_in_time_recovery {
     enabled = local.is_production ? true : false
   }
+  range_key      = "sk"
   read_capacity  = 1 // TODO: Check this value.
   table_class    = local.is_production ? "STANDARD" : "STANDARD_INFREQUENT_ACCESS"
   tags_all       = local.tags_all
