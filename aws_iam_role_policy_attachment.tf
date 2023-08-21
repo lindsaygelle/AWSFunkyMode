5167c606d228c1d8d6e6c859ccf56715588b8f71
lindsaygelle <lindsay.gelle@gmail.com>
2023-08-21 16:57:16 +1000

    Dev

--- aws_iam_role_policy_attachment.tf
+++ aws_iam_role_policy_attachment.tf
@@ -35,6 +35,11 @@ resource "aws_iam_role_policy_attachment" "app_sync_cloud_watch_logs_write_acces
   role       = aws_iam_role.app_sync.id
 }
 
+resource "aws_iam_role_policy_attachment" "app_sync_lambda_cloud_watch_logs_write_access" {
+  policy_arn = aws_iam_policy.cloud_watch_logs_write_access.arn
+  role       = aws_iam_role.app_sync_lambda.id
+}
+
 // CognitoUserPools
 resource "aws_iam_role_policy_attachment" "cognito_lambda_basic_execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole" // TODO
@@ -46,12 +51,10 @@ resource "aws_iam_role_policy_attachment" "cognito_lambda_cloud_watch_logs_write
   role       = aws_iam_role.cognito_lambda.id
 }
 
-
 resource "aws_iam_role_policy_attachment" "cognito_lambda_invoke_function" {
   policy_arn = aws_iam_policy.cognito_lambda_invoke_function.arn
   role       = aws_iam_role.cognito_lambda.id
 }
-
 
 resource "aws_iam_role_policy_attachment" "cognito_mobile_analytics_full_access" {
   policy_arn = aws_iam_policy.mobile_analytics_full_access.arn
@@ -61,5 +64,10 @@ resource "aws_iam_role_policy_attachment" "cognito_mobile_analytics_full_access"
 // DynamoDB
 resource "aws_iam_role_policy_attachment" "dynamo_db_stream_lambda" {
   policy_arn = aws_iam_policy.dynamo_db_stream_read_access.arn
+  role       = aws_iam_role.dynamo_db_stream_lambda.id
+}
+
+resource "aws_iam_role_policy_attachment" "dynamo_db_stream_lambda_cloud_watch_logs_write_access" {
+  policy_arn = aws_iam_policy.cloud_watch_logs_write_access.arn
   role       = aws_iam_role.dynamo_db_stream_lambda.id
 }
