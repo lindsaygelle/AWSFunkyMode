f12d7ea7d0b37da7978bbff1ed7d40be6ca85494
lindsaygelle <lindsay.gelle@gmail.com>
2023-08-28 16:46:54 +1000

    Dev

--- src/cognito/main/lambda_trigger/authentication/post/src/lambda.py
+++ src/cognito/main/lambda_trigger/authentication/post/src/lambda.py
@@ -1,3 +1,36 @@
-def handler(event, context):
-    print(event["request"])
+from typing import Dict, Optional, TypedDict
+
+
+class EventCallerContext(TypedDict):
+    awsSdkVersion: str
+    clientId: str
+
+
+class EventRequestUserAttributes(TypedDict):
+    email: str
+    email_verified: bool
+    sub: str
+
+
+class EventRequest(TypedDict):
+    userAttributes: EventRequestUserAttributes
+    validationData: Optional[Dict]
+
+
+class EventResponse(TypedDict):
+    pass
+
+
+class Event(TypedDict):
+    callerContext: EventCallerContext
+    region: str
+    request: EventRequest
+    response: EventResponse
+    triggerSource: str
+    userName: str
+    userPoolId: str
+    version: str
+
+
+def handler(event: Event, context) -> Event:
     return event
